5c3f5a195ef5f400b8f4b3e7925e75b004f029cb
ardassejose <josevictorps3lima@gmail.com>
2022-08-01 13:22:58 -0300

    Inserido biblioteca Animated.css + Documentação CSS + Melhora na responsividade

--- coletarDados.php
+++ coletarDados.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
   include_once("conexao.php");
 
@@ -31,40 +30,4 @@
   <?php
   }
   ?>
-
-=======
-<?php
-  include_once("conexao.php");
-
-  //parâmetros
-  $nome = $_POST["nomePessoa"];
-  $email = $_POST["emailPessoa"];
-  $telefone = $_POST["telefone"];
-  $descOrcamento = $_POST["descOrcamento"];
-
-  // consulta SQL
-  $sql = "INSERT INTO formulario_orcamento (nomePessoa, emailPessoa, telefone, descOrcamento)";
-  $sql .= "VALUES ('$nome', '$email', '$telefone', '$descOrcamento');";
-
-  echo($sql);
-
-  // conexao
-  if ($conn->query($sql) === TRUE) {
-  ?>
-    <script>
-      alert("Registro salvo com sucesso!");
-      window.location = "index.php";
-    </script>
-  <?php
-  } else {
-  ?>
-    <script>
-      alert("Houve uma falha ao registrar!");
-      window.history.back();
-    </script>
-  <?php
-  }
-  ?>
-
->>>>>>> 04d6be23b7693450db58e05670c0f3446251254b
 ?>
--- conexao.php
+++ conexao.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
   // parâmetros de conexão
   define('HOST','localhost');
@@ -13,20 +12,4 @@
     die('Falha na conexão com banco de dados');
   }
   // echo "Conectado com sucesso";
-=======
-<?php
-  // parâmetros de conexão
-  define('HOST','localhost');
-  define('USER', 'root');
-  define('PASSWORD', '');
-  define('DB', 'landing_page');
-
-  //criar objeto de conexão
-  $conn = new mysqli(HOST, USER, PASSWORD, DB);
-
-  if ($conn -> connect_error){
-    die('Falha na conexão com banco de dados');
-  }
-  // echo "Conectado com sucesso";
->>>>>>> 04d6be23b7693450db58e05670c0f3446251254b
 ?>
